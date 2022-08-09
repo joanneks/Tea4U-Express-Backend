3199d8f71f50287c3ab83fc69ff7a2d841b07618
joanneks <joanne.kan@trentglobal.edu.sg>
2022-08-09 09:23:28 +0000

    added first migration table 'tea' in newly created database 'tea4u'

---
+++ migrations/20220809024759-tea.js
@@ -0,0 +1,41 @@
+'use strict';
+
+var dbm;
+var type;
+var seed;
+
+/**
+  * We receive the dbmigrate dependency from dbmigrate initially.
+  * This enables us to not have to rely on NODE_PATH.
+  */
+exports.setup = function(options, seedLink) {
+  dbm = options.dbmigrate;
+  type = dbm.dataType;
+  seed = seedLink;
+};
+
+exports.up = function(db) {
+  return db.createTable('tea',{
+    id:{type:'int',primaryKey:true,autoIncrement:true,unsigned:true},
+    name:{type:'string',length:50, notNull:true},
+    cost:{type:'smallint',unsigned:true, notNull:true},
+    weight:{type:'smallint',unsigned:true, notNull:true, defaultValue:0},
+    sachet:{type:'smallint',unsigned:true, notNull:true, defaultValue:0},
+    packaging:{type:'string',length:20,notNull:true},
+    image_url:{type:'string',length:255, notNull:true},
+    description:{type:'string',length:400, notNull:true},
+    brew_temperature:{type:'tinyint',unsigned:true, notNull:true},
+    brew_water_quantity:{type:'smallint',unsigned:true,notNull:true},
+    brew_tea_weight:{type:'smallint',unsigned:true,notNull:true},
+    brew_sachet_quantity:{type:'tinyint',unsigned:true,notNull:true},
+    brew_time:{type:'tinyint',unsigned:true,notNull:true}
+  });
+};
+
+exports.down = function(db) {
+  return db.dropTable('tea');
+};
+
+exports._meta = {
+  "version": 1
+};
