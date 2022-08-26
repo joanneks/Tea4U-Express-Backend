d83daec7edb348443c2c86620f808f76af47907b
joanneks <joanne.kan@trentglobal.edu.sg>
2022-08-26 09:25:43 +0000

    deploy again to see if app will crash

--- bookshelf/index.js
+++ bookshelf/index.js
@@ -1,10 +1,23 @@
+// const knex = require('knex')({
+//     client:'mysql',
+//     connection:{
+//         user:process.env.DB_USER,
+//         password:process.env.DB_PASSWORD,
+//         database:process.env.DB_DATABASE,
+//         host:process.env.DB_HOST
+//     }
+// });
+
 const knex = require('knex')({
-    client:'mysql',
+    client:'postgres',
     connection:{
         user:process.env.DB_USER,
         password:process.env.DB_PASSWORD,
         database:process.env.DB_DATABASE,
-        host:process.env.DB_HOST
+        host:process.env.DB_HOST,
+        'ssl': {
+            'rejectUnauthorized': false
+        }
     }
 });
 
